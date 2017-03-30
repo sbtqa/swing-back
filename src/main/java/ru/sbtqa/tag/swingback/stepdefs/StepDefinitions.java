80efe876687cda15939150f49a3ede6a0e31658b
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2017-03-30 18:06:18 +0700

    For tedting only. do not merge it!

--- src/main/java/ru/sbtqa/tag/swingback/stepdefs/StepDefinitions.java
+++ src/main/java/ru/sbtqa/tag/swingback/stepdefs/StepDefinitions.java
@@ -16,7 +16,7 @@ public class StepDefinitions {
     /**
      * Start the application.
      */
-    @And("SBopenApp")
+    @And("ru.sbtqa.tag.swingback.openApp")
     public void startApp() {
         AppManager.getInstance().startApplication();
     }
@@ -24,12 +24,12 @@ public class StepDefinitions {
     /**
      * Closing of application
      */
-    @And("sbCloseApp")
+    @And("ru.sbtqa.tag.swingback.CloseApp")
     public void closeApp() {
         AppManager.getInstance().stopApplication();
     }
 
-    @And("sbOpenForm")
+    @And("ru.sbtqa.tag.swingback.OpenForm")
     public void openForm(String title) {
         AppManager.getInstance().execute(new Callable<Object>() {
             @Override
@@ -46,7 +46,7 @@ public class StepDefinitions {
      * @param pane  pane name
      * @param title tabbed pane title
      */
-    @And("sbMoveToTab")
+    @And("ru.sbtqa.tag.swingback.MoveToTab")
     public void selectTabe(String pane, String title) {
         AppManager.getInstance().execute(new Callable<Object>() {
             @Override
@@ -63,7 +63,7 @@ public class StepDefinitions {
      *
      * @param wait
      */
-    @And("sbWait")
+    @And("ru.sbtqa.tag.swingback.Wait")
     public void wait(String wait) {
         try {
             SECONDS.sleep(Long.parseLong(wait));
@@ -79,7 +79,7 @@ public class StepDefinitions {
      *
      * @param action title of the action to execute
      */
-    @And("sbUserActionNoParams")
+    @And("ru.sbtqa.tag.swingback.UserActionNoParams")
     public void userActionNoParams(String action) {
         AppManager.getInstance().execute(new Callable<Void>() {
             @Override
@@ -97,7 +97,7 @@ public class StepDefinitions {
      * @param action title of the action to execute
      * @param param  parameter
      */
-    @And("sbUserActionOneParam")
+    @And("ru.sbtqa.tag.swingback.UserActionOneParam")
     public void userActionOneParam(String action, String param) {
         AppManager.getInstance().execute(new Callable<Object>() {
             @Override
@@ -116,7 +116,7 @@ public class StepDefinitions {
      * @param param1 first parameter
      * @param param2 second parameter
      */
-    @And("sbUserActionTwoParams")
+    @And("ru.sbtqa.tag.swingback.UserActionTwoParams")
     public void userActionTwoParams(String action, String param1, String param2) {
         AppManager.getInstance().execute(new Callable<Void>() {
             @Override
@@ -136,7 +136,7 @@ public class StepDefinitions {
      * @param param2 second parameter
      * @param param3 third parameter
      */
-    @And("sbUserActionThreeParams")
+    @And("ru.sbtqa.tag.swingback.UserActionThreeParams")
     public void userActionThreeParams(String action, String param1, String param2, String param3) {
         AppManager.getInstance().execute(new Callable<Void>() {
             @Override
@@ -154,7 +154,7 @@ public class StepDefinitions {
      * @param action    title of the action to execute
      * @param dataTable table of parameters
      */
-    @And("sbUserActionTableParam")
+    @And("ru.sbtqa.tag.swingback.UserActionTableParam")
     public void userActionTableParam(String action, DataTable dataTable) {
         AppManager.getInstance().execute(new Callable<Void>() {
             @Override
@@ -173,7 +173,7 @@ public class StepDefinitions {
      * @param param     parameter
      * @param dataTable table of parameters
      */
-    @And("sbUserDoActionWithObject")
+    @And("ru.sbtqa.tag.swingback.UserDoActionWithObject")
     public void userDoActionWithObject(String action, String param, DataTable dataTable) {
         AppManager.getInstance().execute(new Callable<Void>() {
             @Override
